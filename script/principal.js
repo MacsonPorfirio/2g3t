0d704c61ef3c763186568bd91746d0eb2fe56476
macporfirio <110197679+MacsonPorfirio@users.noreply.github.com>
2023-10-02 11:40:40 -0300

    Add files via upload

---
+++ script/principal.js
@@ -0,0 +1,83 @@
+escrever = (msg) => alert(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+div = (a,b) => a / b;
+mult = (a,b) => a * b;
+raiz = a => Math.sqrt(a);
+eq2grau = (a,b,c) => {
+    if(a == 0) return "Não é uma equação do Segundo Grau.";
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+    if(delta < 0) return "Não Possui raiz real.";
+    if(delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x1 " + div(soma(-b,raiz(delta)),mult(2,a)) + 
+           "x2 " + div(sub(-b,raiz(delta)),mult(2,a));
+}
+let a = "";
+let b = "";
+let op = "";
+let resultado = 0;
+let tem_ponto = false;
+let desligada = false;
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    op = "";
+    valor= "";
+    tem_ponto = false;
+    mostra_resultado(0);
+}
+function desligar(){
+    if(desligada){
+        desligada = false;
+
+        zerar();
+    }else{
+        zerar();
+        mostra_resultado("");
+        desligada = true;
+    }
+    return desligada;
+
+    
+}
+
+
+
+
+
+
+
+function mostra_resultado (resultado){
+    if(desligada) return;
+    document.getElementById("resultado"). value = resultado;
+}
+function operacao (nova op){
+    if(desligada) return;
+    op = nova_op;
+    a = valor;
+    valor = "";
+}
+function calcula(){
+    if(desligada) return;
+    if(op != "")
+    b = valor;
+    valor = "";
+    if (op == "soma") mostra_resultado(soma (a,b));
+    if (op == "sub") mostra_resultado(sub (a,b));
+    if (op == "div") mostra_resultado(div (a,b));
+    if (op == "mult") mostra_resultado(mult (a,b));
+}
+function digitando(tecla){
+    if(desligada) return;
+    if(tecla == "."){
+        if(!tem_ponto){
+            vaçor = valor + tecla;
+            mostra_resultado = valor;
+            tem_ponto = true;
+        }
+        return;
+    }
+   valor = valor + tecla;
+   mostra_resultado (valor);
+}
